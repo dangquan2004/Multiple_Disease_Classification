6f41b0c062036d99b7977f0d242660ac3638248c
dangquan2004 <dangquantran@Dangs-MacBook-Pro.local>
2024-10-21 00:49:24 -0400

    All data and files

---
+++ Diabetes Prediction/model_training.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pickle
+from matplotlib import pyplot as plt
+
+# Load csv data into pandas data frame
+diabetes_data = pd.read_csv("diabetes.csv")
+
+# Splitting features and Target
+X = diabetes_data.drop(columns="Outcome", axis=1)
+Y = diabetes_data['Outcome']
+
+
+# Create models:
+def create_model(num_model):
+    model_list = []
+    for i in range(num_model):
+        model = LogisticRegression(max_iter=400)
+        model_list.append(model)
+    return model_list
+
+
+def split_data(X, Y, test_size):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, stratify=Y, random_state=2)
+    return (X_train, Y_train), (X_test, Y_test)
+
+
+def fit_data(model, train_data):
+    model.fit(train_data[0], train_data[1])
+    return model
+
+
+def evaluate_model(model, evaluate_data):
+    X_data, Y_data = evaluate_data
+    X_train_predicton = model.predict(X_data)
+    data_accuracy = accuracy_score(X_train_predicton, Y_data)
+    return data_accuracy
+
+
+def pick_model(test_acc_list, models):
+    best_accuracy = 0
+    best_model = None
+    best_model_index = None
+    for i, accuracy in enumerate(test_acc_list):
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_model = models[i]
+            best_model_index = i
+    return best_model, best_model_index
+
+
+def train_and_pick():
+    models = create_model(10)
+    train_accuracy_list = []
+    test_accuracy_list = []
+    test_size = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    for i in range(len(test_size)):
+        train_data, test_data = split_data(X, Y, test_size[i])
+        model = fit_data(models[i], train_data)
+        train_data_accuracy = evaluate_model(model, train_data)
+        test_data_accuracy = evaluate_model(model, test_data)
+        test_accuracy_list.append(test_data_accuracy)
+        train_accuracy_list.append(train_data_accuracy)
+        models.append(model)
+
+    plt.plot(test_size, test_accuracy_list, )
+    best_model, best_model_index = pick_model(test_accuracy_list, models)
+    print(f"Model #{best_model_index} has the highest prediction with {test_accuracy_list[best_model_index] * 100:.2f}%")
+    return best_model
+
+
+def make_prediction(model, input_data):
+    input_data_as_arr = np.asarray(input_data)
+    input_data_reshape = input_data_as_arr.reshape(1, -1)
+    prediction = model.predict(input_data_reshape)
+
+    if prediction[0] == 0:
+        print("The person does not have Diabetes!")
+    else:
+        print("The person has Diabetes")
+
+
+def save_model(model, file_name):
+    pickle.dump(model, open(file_name, 'wb'))
+
+
+best_model = train_and_pick()
+save_model(best_model, "../saved_models/diabetes_model.sav")
